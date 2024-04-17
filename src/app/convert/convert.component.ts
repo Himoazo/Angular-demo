ec099ce26ab8aff28334bbbcdb21378089d96130
Hoomam <hoaz1600@student.miun.se>
2024-04-17 17:54:07 +0200

    conversions

--- src/app/convert/convert.component.ts
+++ src/app/convert/convert.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convert.component.html',
   styleUrl: './convert.component.css'
 })
 export class ConvertComponent {
+  footNum: number = 0;
+  meterNum: number = 0;
+  celNum: number = 0;
+  fahrNum: number = 0;
 
+  footToMeter(): void {
+    this.meterNum = this.footNum * 0.3048;
+  }
+
+  meterToFeet(): void {
+    this.footNum = this.meterNum * 3.28084;
+  }
+
+  celToFahr(): void {
+    this.fahrNum = (this.celNum * 1.8) +32;
+  }
+
+  farhToCel(): void {
+    this.celNum = (this.fahrNum - 32) * 5/9;
+  }
 }
